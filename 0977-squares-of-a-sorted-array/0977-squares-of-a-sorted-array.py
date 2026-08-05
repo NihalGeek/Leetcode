d989a113492be17d0f65b7cf6b31db92842ceb82
Nihal Somarajupalli <161623461+NihalGeek@users.noreply.github.com>
2026-08-05 14:49:27 +0530

    Time: 14 ms (28.39%), Space: 21.3 MB (9.79%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -7,20 +7,19 @@ class Solution:
                 neg.append(i)
             else:
                 pos.append(i)
-            
-        i=j=0
+        
         for i in range(len(neg)):
             neg[i]*=neg[i]
+        neg.reverse()
 
         for j in range(len(pos)):
             pos[j]*=pos[j]
-        
-        neg.reverse()
 
-        res=[]
-        i=j=0
+        i=0
+        j=0
         n=len(neg)
         m=len(pos)
+        res=[]
 
         while (i<n and j<m):
             if neg[i]<pos[j]:
@@ -29,13 +28,18 @@ class Solution:
             else:
                 res.append(pos[j])
                 j+=1
-            
+
         while i<n:
             res.append(neg[i])
             i+=1
-        
+
         while j<m:
             res.append(pos[j])
             j+=1
         
         return res
+
+
+        
+
+        
