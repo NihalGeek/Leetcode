9294526a591f4ddc3009b39931d163de1c990c41
Nihal Somarajupalli <161623461+NihalGeek@users.noreply.github.com>
2026-08-08 22:07:31 +0530

    Time: 472 ms (67.39%), Space: 46.9 MB (80.43%) - LeetHub

--- 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.py
+++ 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.py
@@ -3,15 +3,11 @@ from typing import List
 class Solution:
     def validSequence(self, word1: str, word2: str) -> List[int]:
 
-        # last[j] = the rightmost index in word1
-        # that can be used to match word2[j]
-        # while matching word2[j:] from right to left.
         last = [-1] * len(word2)
 
         i = len(word1) - 1
         j = len(word2) - 1
 
-        # Build suffix information
         while i >= 0 and j >= 0:
 
             if word1[i] == word2[j]:
@@ -20,32 +16,22 @@ class Solution:
 
             i -= 1
 
-        # canSkip means:
-        # we have NOT used our one allowed mismatch yet.
         canSkip = True
 
         ans = []
         j = 0
 
-        # Greedily scan word1 from left to right
         for i in range(len(word1)):
 
-            # We already selected all characters we need
             if j == len(word2):
                 break
 
-            # Case 1:
-            # Current character matches exactly.
+            
             if word1[i] == word2[j]:
                 ans.append(i)
                 j += 1
 
-            # Case 2:
-            # Current character doesn't match.
-            #
-            # We can use our one allowed mismatch,
-            # BUT only if there is enough room to match
-            # the remaining word2 exactly.
+            
             elif canSkip and (
                 j == len(word2) - 1
                 or i < last[j + 1]
@@ -54,7 +40,6 @@ class Solution:
                 ans.append(i)
                 j += 1
 
-        # We successfully selected every character of word2
         if j == len(word2):
             return ans
 
