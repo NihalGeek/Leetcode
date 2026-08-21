2e72712e0d53a0074fb6c2674ba6da1a27347530
Nihal Somarajupalli <161623461+NihalGeek@users.noreply.github.com>
2026-08-21 15:07:19 +0530

    Time: 3 ms (6.26%), Space: 19.3 MB (38.8%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -6,7 +6,7 @@ class Solution:
         i=m-1
         j=n-1
         k=m+n-1
-        while (i>=0 and j>=0):
+        while i>=0 and j>=0:
             if nums1[i]>nums2[j]:
                 nums1[k]=nums1[i]
                 i-=1
@@ -14,8 +14,16 @@ class Solution:
                 nums1[k]=nums2[j]
                 j-=1
             k-=1
+
+        while i>=0:
+            nums1[k]=nums1[i]
+            i-=1
+            k-=1
         
         while j>=0:
             nums1[k]=nums2[j]
             j-=1
             k-=1
+        
+
+        
