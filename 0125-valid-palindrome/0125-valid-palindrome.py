e75e528823fa8f73df90b75aab1af11df199e7ff
Nihal Somarajupalli <161623461+NihalGeek@users.noreply.github.com>
2026-09-23 09:13:57 +0530

    Time: 11 ms (31.55%), Space: 19.5 MB (78.64%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -4,6 +4,7 @@ class Solution:
         for i in s:
             if i.isalnum():
                 res+=i.lower()
+        
         left=0
         right=len(res)-1
         while left<=right:
@@ -12,11 +13,5 @@ class Solution:
             left+=1
             right-=1
         return True
-        
-
-    
-
-
-
 
         
