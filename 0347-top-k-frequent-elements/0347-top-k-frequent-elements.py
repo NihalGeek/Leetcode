5f942daa79bf865b6de7fb29197a238a484e93ee
Nihal Somarajupalli <161623461+NihalGeek@users.noreply.github.com>
2026-09-26 12:11:31 +0530

    Time: 7 ms (52.27%), Space: 22.8 MB (61.2%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -6,8 +6,8 @@ class Solution:
                 freq[i]=1
             else:
                 freq[i]+=1
-            
-        sorted_list=sorted(freq.items(),key=lambda x:x[1],reverse = True)
+        
+        sorted_list=sorted(freq.items(),key=lambda x:x[1],reverse=True)
         res=[]
         for i in range(k):
             res.append(sorted_list[i][0])
